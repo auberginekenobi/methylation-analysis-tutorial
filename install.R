0f4b8237e2fd592966f93bafcb98f2197d7bb7de
Owen Chapman <ochapman@eng.ucsd.edu>
2023-11-08 13:41:33 -0800

    Update install.R

--- install.R
+++ install.R
@@ -1,9 +1,9 @@
-if (!require("BiocManager", quietly = TRUE)){
-    install.packages("BiocManager")
-}
-BiocManager::install("minfi")
-BiocManager::install("minfiData")
-BiocManager::install("conumee")
-BiocManager::install("Rtsne")
-BiocManager::install("sva")
+#if (!require("BiocManager", quietly = TRUE)){
+#    install.packages("BiocManager")
+#}
+#BiocManager::install("minfi")
+#BiocManager::install("minfiData")
+#BiocManager::install("conumee")
+#BiocManager::install("Rtsne")
+#BiocManager::install("sva")
 install.packages("tictoc")
