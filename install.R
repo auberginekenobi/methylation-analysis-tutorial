e4c93c28feeac7ef7527951ebf67c38c4e68b35e
Owen Chapman <owenchapman1@gmail.com>
2025-05-02 13:19:47 +0900

    Remove unnecessary files

--- install.R
+++
@@ -1,9 +0,0 @@
-if (!require("BiocManager", quietly = TRUE)){
-    install.packages("BiocManager")
-}
-BiocManager::install("minfi")
-BiocManager::install("minfiData")
-BiocManager::install("conumee")
-BiocManager::install("Rtsne")
-BiocManager::install("sva")
-install.packages("tictoc")
